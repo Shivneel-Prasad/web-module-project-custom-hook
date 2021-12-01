902ecf380b71a309cc331a564a039cafbdfed10a
Shivneel-Prasad <Shivam201020@gmail.com>
2021-12-01 11:21:53 -0800

    updated both files in the Hook

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = (initialValues) => {
+    const [values, setValues] = useLocalStorage('darkmode',initialValues)
+
+    return (
+        [values, setValues]
+    )
+}
+
+export default useDarkMode;
