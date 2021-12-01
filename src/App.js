4c2fd5b3468d8920f7902d8e9c076b8e2b696660
Shivneel-Prasad <Shivam201020@gmail.com>
2021-12-01 11:26:39 -0800

    Assignment Complete

--- src/App.js
+++ src/App.js
@@ -12,10 +12,7 @@ const App = () => {
   const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
+    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true")
       .then(res => setCoinData(res.data))
       .catch(err => console.log(err));
   }, []);
--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,9 +3,8 @@ import useLocalStorage from "./useLocalStorage";
 const useDarkMode = (initialValues) => {
     const [values, setValues] = useLocalStorage('darkmode',initialValues)
 
-    return (
-        [values, setValues]
-    )
+    return [values, setValues];
+    
 }
 
 export default useDarkMode;
